f297d5a970ad38510075466d946b2e2f9284f434
Joakim Bjerselius <176925453+jockeBjers@users.noreply.github.com>
2024-10-21 13:50:16 +0200

    make new js file to avoid loading wrong pages causing error

---
+++ pages/portfolio.js
@@ -0,0 +1,70 @@
+
+/* portfolio page */
+const iframes = [
+    {
+        src: "https://jockebjers.github.io/LoginForm/",
+        link: "https://github.com/jockeBjers/Loginform",
+        title: "Sign up form"
+    },
+
+    {
+        src: "https://jockebjers.github.io/rock-paper-scissors/",
+        link: "https://github.com/jockeBjers/rock-paper-scissors",
+        title: "Rock - Paper - Scissors"
+    },
+
+    {
+        src: "https://jockebjers.github.io/Etch-a-Sketch/",
+        link: "https://github.com/jockeBjers/Etch-a-Sketch",
+        title: "Etch - a - sketch"
+    },
+    {
+        src: "../images/Screenshot_163.png",
+        link: "https://github.com/jockeBjers/StudentManagementSystem",
+        title: "Student management system"
+    }
+];
+
+let currentIframeIndex = 0;
+const iframeElement = document.getElementById("portfolio-iframe");
+const linkElement = document.getElementById("portfolio-link");
+const prevButton = document.getElementById("portfolio-prev");
+const nextButton = document.getElementById("portfolio-next");
+function updateIframe() {
+
+
+    iframeElement.src = iframes[currentIframeIndex].src;
+    linkElement.href = iframes[currentIframeIndex].link;
+    linkElement.textContent = iframes[currentIframeIndex].title;
+
+    if (currentIframeIndex === 0) {
+        prevButton.style.visibility = 'hidden';
+    } else {
+        prevButton.style.visibility = 'visible';
+    }
+
+    if (currentIframeIndex === iframes.length - 1) {
+        nextButton.style.visibility = 'hidden';
+    } else {
+        nextButton.style.visibility = 'visible';
+    }
+}
+
+
+function nextIframe() {
+    if (currentIframeIndex < iframes.length - 1) {
+        currentIframeIndex++;
+        updateIframe();
+    }
+}
+
+function previousIframe() {
+    if (currentIframeIndex > 0) {
+        currentIframeIndex--;
+        updateIframe();
+    }
+}
+
+currentIframeIndex = 0;
+updateIframe();    
+
